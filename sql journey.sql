efcb115fa7d1f263c2191b2e6ee61c95e03d3958
Retji <37171086+Retji@users.noreply.github.com>
2022-04-28 10:51:34 +0100

    Update sql journey.sql

--- sql journey.sql
+++ sql journey.sql
@@ -1,5 +1,5 @@
 show databases;
-create database db_database;
+CREATE database db_database;
 use db_database;
 #creating my first table with the name people consisting of three columns
 CREATE TABLE people (first_name VARCHAR(20), last_name VARCHAR(20), age INT);
@@ -20,16 +20,22 @@ CREATE TABLE dog_db (name VARCHAR(50) NOT NULL default 'unnamed', age INT NOT NU
 # to create a table with the default of unnamed if no name provided
 INSERT INTO dog_db (age) VALUES(10);
 INSERT INTO dog_db  VALUES ();
+
 SELECT * FROM dog_db;
-INSERT INTO dog_db VALUES('nAPO',12);
+
+INSERT INTO dog_db VALUES('nAPO',12); #adding another row into the database dog_db
 
 CREATE TABLE dog_db2 (id_no INT auto_increment, name VARCHAR(20) NOT NULL, age INT, PRIMARY KEY(id_no));
+
+#the above syntax creates a new table dog_db2 with an autoincremental id, i.e the values of the id increases incrementally
+
 INSERT INTO dog_db2(name,age) VALUES ('Snow ball',2);
 INSERT INTO dog_db2(name, age) VALUES('SERI',5);
 INSERT INTO dog_db2(name,age) VALUES('Chariz', 7);
 
 select * from dog_db2;
-DROP TABLE dog_db2; 
+DROP TABLE dog_db2;     # to drop the dog_db2 from the databases
+
 CREATE TABLE dog_db2 (id_no INT auto_increment PRIMARY KEY, name VARCHAR(20) NOT NULL, Breed VARCHAR(50));
 INSERT INTO dog_db2 (name,Breed) VALUES
 ('Ringo','Caucasian'),
@@ -37,4 +43,4 @@ INSERT INTO dog_db2 (name,Breed) VALUES
 ('salitary','Tabby'),
 ('Abadiki', 'wolf pegg');
 
-
+#insert multiple values into the new dog_db2 table simultaneously
